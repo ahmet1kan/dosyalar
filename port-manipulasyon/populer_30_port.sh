afa9aa160a0d5f3a7e30af0390aa39b4ccdd5ff5
ahmet1kan <ahmet1kan@outlook.com>
2024-10-22 23:02:14 +0300

    Add files via upload

---
+++ port-manipulasyon/populer_30_port.sh
@@ -0,0 +1,60 @@
+echo "Test Port : 20"
+telnet 10.10.9.65 20
+echo "Test Port : 21"
+telnet 10.10.9.65 21
+echo "Test Port : 22"
+telnet 10.10.9.65 22
+echo "Test Port : 23"
+telnet 10.10.9.65 23
+echo "Test Port : 25"
+telnet 10.10.9.65 25
+echo "Test Port : 53"
+telnet 10.10.9.65 53
+echo "Test Port : 80"
+telnet 10.10.9.65 80
+echo "Test Port : 110"
+telnet 10.10.9.65 110
+echo "Test Port : 143"
+telnet 10.10.9.65 143
+echo "Test Port : 443"
+telnet 10.10.9.65 443
+echo "Test Port : 445"
+telnet 10.10.9.65 445
+echo "Test Port : 465"
+telnet 10.10.9.65 465
+echo "Test Port : 587"
+telnet 10.10.9.65 587
+echo "Test Port : 993"
+telnet 10.10.9.65 993
+echo "Test Port : 995"
+telnet 10.10.9.65 995
+echo "Test Port : 1025"
+telnet 10.10.9.65 1025
+echo "Test Port : 135"
+telnet 10.10.9.65 135
+echo "Test Port : 139"
+telnet 10.10.9.65 139
+echo "Test Port : 1433"
+telnet 10.10.9.65 1433
+echo "Test Port : 1434"
+telnet 10.10.9.65 1434
+echo "Test Port : 1521"
+telnet 10.10.9.65 1521
+echo "Test Port : 3306"
+telnet 10.10.9.65 3306
+echo "Test Port : 3389"
+telnet 10.10.9.65 3389
+echo "Test Port : 5060"
+telnet 10.10.9.65 5060
+echo "Test Port : 5900"
+telnet 10.10.9.65 5900
+echo "Test Port : 8080"
+telnet 10.10.9.65 8080
+echo "Test Port : 8888"
+telnet 10.10.9.65 8888
+echo "Test Port : 10000"
+telnet 10.10.9.65 10000
+echo "Test Port : 32768"
+telnet 10.10.9.65 32768
+echo "Test Port : 49152"
+telnet 10.10.9.65 49152
